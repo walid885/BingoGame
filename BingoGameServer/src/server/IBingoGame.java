9663c551a2664b677f2b09d91fdd2cd0d8a4aa4c
walid885 <bettaieb88walid@gmail.com>
2024-11-26 15:42:40 +0100

    interface new , for workign

--- BingoGameServer/src/server/IBingoGame.java
+++ BingoGameServer/src/server/IBingoGame.java
@@ -5,16 +5,19 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 
+
 public interface IBingoGame extends Serializable {
     boolean makeGuess(int playerId, int number) throws RemoteException;
     int startNewGame() throws RemoteException;
     int getHighestScore() throws RemoteException;
-    GameStatus getGameStatus(int playerId) throws RemoteException;
 
-    // Utilisez GameStatus du serveur
-    enum GameStatus {
-        IN_PROGRESS,
-        COMPLETED,
-        NOT_STARTED
-    }
+    String getGameStatus(int playerId) throws RemoteException;
+    
+    // Remove the enum or replace with constants
+    class GameStatus {
+        public static final String IN_PROGRESS = "IN_PROGRESS";
+        public static final String COMPLETED = "COMPLETED";
+        public static final String NOT_STARTED = "NOT_STARTED";
+
+}
 }
